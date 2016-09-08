c085633c61bfa8c431b5cddb9566cb790aa85603
HJ Lee <hj.d.lee@gmail.com>
2016-09-08 15:07:02 -0400

    Reindent, new default angleX

--- scripts/appthree.js
+++ scripts/appthree.js
@@ -1,8 +1,8 @@
+// (function () {
+
 // fork getUserMedia for multiple browser versions, for those
 // that need prefixes
 
-// (function () {
-  
 navigator.getUserMedia = (navigator.getUserMedia ||
                           navigator.webkitGetUserMedia ||
                           navigator.mozGetUserMedia ||
@@ -46,32 +46,32 @@ var drawVisual;
 // connect audio to analyser
 
 if (navigator.getUserMedia) {
-   console.log('getUserMedia supported.');
-   navigator.getUserMedia (
-      // constraints - only audio needed for this app
-      {
-         audio: true
-      },
+    console.log('getUserMedia supported.');
+    navigator.getUserMedia (
+	// constraints - only audio needed for this app
+	{
+            audio: true
+	},
 
-      // Success callback
-      function(stream) {
-        source = audioCtx.createMediaStreamSource(stream);
-        console.log('success callback - ' + source.context.sampleRate);
+	// Success callback
+	function(stream) {
+            source = audioCtx.createMediaStreamSource(stream);
+            console.log('success callback - ' + source.context.sampleRate);
 
-	sampleRateElm.innerText = source.context.sampleRate;
-	
-         source.connect(analyser);
+	    sampleRateElm.innerText = source.context.sampleRate;
+	    
+            source.connect(analyser);
 
-      	 visualize();
-      },
+      	    visualize();
+	},
 
-      // Error callback
-      function(err) {
-         console.log('The following gUM error occured: ' + err);
-      }
-   );
+	// Error callback
+	function(err) {
+            console.log('The following gUM error occured: ' + err);
+	}
+    );
 } else {
-   console.log('getUserMedia not supported on your browser!');
+    console.log('getUserMedia not supported on your browser!');
 }
 
 /////////////////////////////////////
@@ -100,17 +100,17 @@ var camera = new THREE.PerspectiveCamera(15, WIDTH / HEIGHT, 1, WIDTH * 3);
 DISTANCE_FACTOR = 2.1
 
 
-var angleX = Math.PI/4;
+var angleX = Math.PI/6;
 var angleY = 0;
 
 function setCameraAngle(angleX, angleY) {
-  camera.position.x = WIDTH/2 + WIDTH * DISTANCE_FACTOR * Math.sin(angleY);
-  camera.position.y = HEIGHT/3 + WIDTH * DISTANCE_FACTOR * Math.sin(angleX) * Math.cos(angleY);
-  camera.position.z = ZSTEP*75 + WIDTH * DISTANCE_FACTOR * Math.cos(angleX) * Math.cos(angleY);
+    camera.position.x = WIDTH/2 + WIDTH * DISTANCE_FACTOR * Math.sin(angleY);
+    camera.position.y = HEIGHT/3 + WIDTH * DISTANCE_FACTOR * Math.sin(angleX) * Math.cos(angleY);
+    camera.position.z = ZSTEP*75 + WIDTH * DISTANCE_FACTOR * Math.cos(angleX) * Math.cos(angleY);
 
 
-  camera.rotation.x = - angleX;
-  camera.rotation.y = angleY;
+    camera.rotation.x = - angleX;
+    camera.rotation.y = angleY;
 }  
 
 setCameraAngle(angleX, angleY);
@@ -124,27 +124,27 @@ MAX_ANGLE_Y = Math.PI/2;
 
 
 document.addEventListener('keydown', function(event) {
-  var code = event.code;
-  if (code == 'KeyW') {
-    angleX += ANGLE_STEP;
-    if (angleX > MAX_ANGLE_X) angleX = MAX_ANGLE_X;
-    setCameraAngle(angleX, angleY);
-  }
-  else if (code == 'KeyS') {
-    angleX -= ANGLE_STEP;
-    if (angleX < MIN_ANGLE_X) angleX = MIN_ANGLE_X;
-    setCameraAngle(angleX, angleY);
-  }
-  else if (code == 'KeyA') {
-    angleY -= ANGLE_STEP;
-    if (angleY < MIN_ANGLE_Y) angleY = MIN_ANGLE_Y;
-    setCameraAngle(angleX, angleY);
-  }
-  else if (code == 'KeyD') {
-    angleY += ANGLE_STEP;
-    if (angleY > MAX_ANGLE_Y) angleY = MAX_ANGLE_Y;
-    setCameraAngle(angleX, angleY);
-  }
+    var code = event.code;
+    if (code == 'KeyW') {
+	angleX += ANGLE_STEP;
+	if (angleX > MAX_ANGLE_X) angleX = MAX_ANGLE_X;
+	setCameraAngle(angleX, angleY);
+    }
+    else if (code == 'KeyS') {
+	angleX -= ANGLE_STEP;
+	if (angleX < MIN_ANGLE_X) angleX = MIN_ANGLE_X;
+	setCameraAngle(angleX, angleY);
+    }
+    else if (code == 'KeyA') {
+	angleY -= ANGLE_STEP;
+	if (angleY < MIN_ANGLE_Y) angleY = MIN_ANGLE_Y;
+	setCameraAngle(angleX, angleY);
+    }
+    else if (code == 'KeyD') {
+	angleY += ANGLE_STEP;
+	if (angleY > MAX_ANGLE_Y) angleY = MAX_ANGLE_Y;
+	setCameraAngle(angleX, angleY);
+    }
 });
 
 
@@ -157,15 +157,15 @@ var drawStyleFunctions = {}
 
 // common functions
 function lineMaterial(color) {
-  return new THREE.LineBasicMaterial({
-    color: color
-  });  
+    return new THREE.LineBasicMaterial({
+	color: color
+    });  
 }
 
 function meshMaterial(color) {
-  return new THREE.MeshBasicMaterial({
-    color: color
-  });  
+    return new THREE.MeshBasicMaterial({
+	color: color
+    });  
 }
 
 // line
@@ -175,11 +175,11 @@ drawStyleFunctions["line"] = {}
 drawStyleFunctions["line"].makeMaterial = lineMaterial;
 
 drawStyleFunctions["line"].makeObject =
-  function(prevVectorArry, vectorArray, material)
+    function(prevVectorArry, vectorArray, material)
 {
-  var geometry = new THREE.Geometry();
-  geometry.vertices = vectorArray;
-  return new THREE.Line(geometry, material);
+    var geometry = new THREE.Geometry();
+    geometry.vertices = vectorArray;
+    return new THREE.Line(geometry, material);
 }
 
 // frontmesh
@@ -189,26 +189,26 @@ drawStyleFunctions["frontmesh"] = {}
 drawStyleFunctions["frontmesh"].makeMaterial = meshMaterial;
 
 drawStyleFunctions["frontmesh"].makeObject =
-  function(prevVectorArry, vectorArray, material)
+    function(prevVectorArry, vectorArray, material)
 {
-  var geometry = new THREE.Geometry();
-  for(var i = 0; i < vectorArray.length; i++) {
-    var vertex = vectorArray[i];
-    geometry.vertices.push(
-      new THREE.Vector3(vertex.x, 0, 0)
-    );
-    vertex.y += 2;
-    geometry.vertices.push(vertex);
-    if (i>0) {
-      geometry.faces.push(
-	new THREE.Face3(i*2, i*2-1, i*2-2)
-      );
-      geometry.faces.push(
-	new THREE.Face3(i*2+1, i*2-1, i*2)
-      );
+    var geometry = new THREE.Geometry();
+    for(var i = 0; i < vectorArray.length; i++) {
+	var vertex = vectorArray[i];
+	geometry.vertices.push(
+	    new THREE.Vector3(vertex.x, 0, 0)
+	);
+	vertex.y += 2;
+	geometry.vertices.push(vertex);
+	if (i>0) {
+	    geometry.faces.push(
+		new THREE.Face3(i*2, i*2-1, i*2-2)
+	    );
+	    geometry.faces.push(
+		new THREE.Face3(i*2+1, i*2-1, i*2)
+	    );
+	}
     }
-  }
-  return new THREE.Mesh(geometry, material);
+    return new THREE.Mesh(geometry, material);
 }
 
 // upmesh
@@ -218,25 +218,25 @@ drawStyleFunctions["upmesh"] = {}
 drawStyleFunctions["upmesh"].makeMaterial = meshMaterial;
 
 drawStyleFunctions["upmesh"].makeObject =
-  function(prevVectorArry, vectorArray, material)
+    function(prevVectorArry, vectorArray, material)
 {
-  if (prevVectorArry) {
-    var geometry = new THREE.Geometry();
-    for(var i = 0; i < vectorArray.length; i++) {
-      prevVectorArry[i].z = ZSTEP;
-      geometry.vertices.push(vectorArray[i]);
-      geometry.vertices.push(prevVectorArry[i]);
-      if (i>0) {
-      geometry.faces.push(
-	new THREE.Face3(i*2, i*2-1, i*2-2)
-      );
-      geometry.faces.push(
-	new THREE.Face3(i*2+1, i*2-1, i*2)
-      );
-      }
+    if (prevVectorArry) {
+	var geometry = new THREE.Geometry();
+	for(var i = 0; i < vectorArray.length; i++) {
+	    prevVectorArry[i].z = ZSTEP;
+	    geometry.vertices.push(vectorArray[i]);
+	    geometry.vertices.push(prevVectorArry[i]);
+	    if (i>0) {
+		geometry.faces.push(
+		    new THREE.Face3(i*2, i*2-1, i*2-2)
+		);
+		geometry.faces.push(
+		    new THREE.Face3(i*2+1, i*2-1, i*2)
+		);
+	    }
+	}
+	return new THREE.Mesh(geometry, material);
     }
-    return new THREE.Mesh(geometry, material);
-  }
 }
 
 // bar
@@ -244,64 +244,64 @@ drawStyleFunctions["upmesh"].makeObject =
 
 barMaterials = new Array(256/4);
 for(var i = 0; i < barMaterials.length; i++) {
-  var base = 80 * 256;
-  if (i%2 == 0) base = 80;
-  var c = i * 4 * 256 * 256 + base;
-  // var c = (120 + i * 2) *(1+256+256*256);
-  barMaterials[i] = new THREE.MeshBasicMaterial({
-    color: c
-  });
+    var base = 80 * 256;
+    if (i%2 == 0) base = 80;
+    var c = i * 4 * 256 * 256 + base;
+    // var c = (120 + i * 2) *(1+256+256*256);
+    barMaterials[i] = new THREE.MeshBasicMaterial({
+	color: c
+    });
 }
 
 drawStyleFunctions["bar"] = {}
 drawStyleFunctions["bar"].makeMaterial = meshMaterial;
 
 drawStyleFunctions["bar"].makeObject =
-  function(prevVectorArry, vectorArray, material)
+    function(prevVectorArry, vectorArray, material)
 {
-  var geometryArray = new Array(256/4);
-  for(var i = 0; i < geometryArray.length; i++) {
-    geometryArray[i] = new THREE.Geometry();
-  }
-  var group = new THREE.Group();
-  var max = 0;
-  for(var i = 0; i < vectorArray.length-1; i++) {
-    var vertex = vectorArray[i];
-    var nextVertex = vectorArray[i+1];
-
-    var idx = Math.floor(vertex.y/4);
-    idx = Math.min(idx, geometryArray.length-1);
-
-    vertex.y += 2;
-    
-    geometryArray[idx].vertices.push(
-      new THREE.Vector3(vertex.x, 0, 0)
-    );
-    geometryArray[idx].vertices.push(vertex);
-    geometryArray[idx].vertices.push(
-      new THREE.Vector3(nextVertex.x, 0, 0)
-    );
-    geometryArray[idx].vertices.push(
-      new THREE.Vector3(nextVertex.x, vertex.y, 0)
-    );
-    
-    var i4 = geometryArray[idx].vertices.length - 4;
-    geometryArray[idx].faces.push(
-      new THREE.Face3(i4+2, i4+1, i4+0)
-    );
-    geometryArray[idx].faces.push(
-      new THREE.Face3(i4+3, i4+1, i4+2)
-    );
-  }
-  // if (max > 255) max = 255;
-  // return new THREE.Mesh(geometry, barMaterials[Math.floor(max/4)]);
-  for(var i = 0; i < geometryArray.length; i++) {
-    if (geometryArray[i].vertices.length > 0) {
-      group.add(new THREE.Mesh(geometryArray[i], barMaterials[i]));
+    var geometryArray = new Array(256/4);
+    for(var i = 0; i < geometryArray.length; i++) {
+	geometryArray[i] = new THREE.Geometry();
     }
-    geometryArray[i].dispose();
-  }
-  return group;
+    var group = new THREE.Group();
+    var max = 0;
+    for(var i = 0; i < vectorArray.length-1; i++) {
+	var vertex = vectorArray[i];
+	var nextVertex = vectorArray[i+1];
+
+	var idx = Math.floor(vertex.y/4);
+	idx = Math.min(idx, geometryArray.length-1);
+
+	vertex.y += 2;
+	
+	geometryArray[idx].vertices.push(
+	    new THREE.Vector3(vertex.x, 0, 0)
+	);
+	geometryArray[idx].vertices.push(vertex);
+	geometryArray[idx].vertices.push(
+	    new THREE.Vector3(nextVertex.x, 0, 0)
+	);
+	geometryArray[idx].vertices.push(
+	    new THREE.Vector3(nextVertex.x, vertex.y, 0)
+	);
+	
+	var i4 = geometryArray[idx].vertices.length - 4;
+	geometryArray[idx].faces.push(
+	    new THREE.Face3(i4+2, i4+1, i4+0)
+	);
+	geometryArray[idx].faces.push(
+	    new THREE.Face3(i4+3, i4+1, i4+2)
+	);
+    }
+    // if (max > 255) max = 255;
+    // return new THREE.Mesh(geometry, barMaterials[Math.floor(max/4)]);
+    for(var i = 0; i < geometryArray.length; i++) {
+	if (geometryArray[i].vertices.length > 0) {
+	    group.add(new THREE.Mesh(geometryArray[i], barMaterials[i]));
+	}
+	geometryArray[i].dispose();
+    }
+    return group;
 }
 drawStyleFunctions["bar"].skipMaterialChange = true;
 
@@ -310,179 +310,179 @@ drawStyleFunctions["bar"].skipMaterialChange = true;
 // rendering
 
 function deepDispose(obj) {
-  if (obj.geometry) obj.geometry.dispose();
-  // group?
-  if (obj.traverse) {
-    obj.traverse(function(subObj) {
-      if(obj.id != subObj.id) deepDispose(subObj);
-    });
-  }
-  if (obj.dispose) obj.dispose();
+    if (obj.geometry) obj.geometry.dispose();
+    // group?
+    if (obj.traverse) {
+	obj.traverse(function(subObj) {
+	    if(obj.id != subObj.id) deepDispose(subObj);
+	});
+    }
+    if (obj.dispose) obj.dispose();
 }
 
 function visualize() {
-  analyser.fftSize = Number(fftSizeSelect.value);  
-  var NARRAY = Number(nlinesSelect.value);
-  var bufferLength = analyser.frequencyBinCount;
-  console.log(bufferLength);
-  var drawStyle = styleSelect.value;
+    analyser.fftSize = Number(fftSizeSelect.value);  
+    var NARRAY = Number(nlinesSelect.value);
+    var bufferLength = analyser.frequencyBinCount;
+    console.log(bufferLength);
+    var drawStyle = styleSelect.value;
 
-  // stop rendering
-  if (drawStyle == "off") return;
-  
-  var frameLength = analyser.fftSize / audioCtx.sampleRate;
-  frameLengthElm.innerText = frameLength;
+    // stop rendering
+    if (drawStyle == "off") return;
+    
+    var frameLength = analyser.fftSize / audioCtx.sampleRate;
+    frameLengthElm.innerText = frameLength;
 
-  
-  var dataArray = new Uint8Array(bufferLength);  
+    
+    var dataArray = new Uint8Array(bufferLength);  
 
-  var objectArray = new Array(NARRAY);
-  
-  scene = new THREE.Scene();
+    var objectArray = new Array(NARRAY);
+    
+    scene = new THREE.Scene();
 
-  var material;
-  material = drawStyleFunctions[drawStyle].makeMaterial(0xffffff);
+    var material;
+    material = drawStyleFunctions[drawStyle].makeMaterial(0xffffff);
 
-  // dispose old oldMaterials
-  if (oldMaterials) {
-    oldMaterials.forEach(function(m) { if(m.dispose) m.dispose(); });
-  }
-
-  // rebuild oldMaterials
-  oldMaterials = new Array(NARRAY);
-  for(var i = 0; i < NARRAY; i++) {
-    var addColor = Math.floor(256 * (i/NARRAY));
-    if (i % 2 == 0)
-      addColor = Math.floor(256 * 256 * 256 * ((NARRAY-i)/NARRAY));
-    var c = 256 * 125 + addColor;
-    oldMaterials[i] = drawStyleFunctions[drawStyle].makeMaterial(c);
-  }
-
-  // draw() sets
-  var arrayIdx = 0;
-  var prevVectorArry;
-
-  function draw() {
-    drawVisual = requestAnimationFrame(draw);
-
-    {
-      // remove old object
-      var oldObj = objectArray[(arrayIdx + 1)%NARRAY];
-      if (oldObj) {
-	scene.remove(oldObj);
-	deepDispose(oldObj);
-      }
-      // move objects backward
-      scene.traverse(function(obj) {
-	if(scene.id != obj.id) {
-	  obj.translateZ(ZSTEP);
-	}
-      });
-      // change last object material
-      if (!drawStyleFunctions[drawStyle].skipMaterialChange) {
-      	var prevObj = objectArray[(arrayIdx + NARRAY -1) % NARRAY];
-      	if (prevObj) {
-      	  prevObj.material = oldMaterials[arrayIdx];
-      	}
-      }
+    // dispose old oldMaterials
+    if (oldMaterials) {
+	oldMaterials.forEach(function(m) { if(m.dispose) m.dispose(); });
     }
-    
-    analyser.getByteFrequencyData(dataArray);
 
-    var maxDrawFreq = MAX_FREQ / (source.context.sampleRate / analyser.fftSize);
-    maxDrawFreq = Math.min(maxDrawFreq, bufferLength);
-    var unitWidth = (WIDTH / maxDrawFreq);
-    
-    var vectorArray = new Array();
+    // rebuild oldMaterials
+    oldMaterials = new Array(NARRAY);
+    for(var i = 0; i < NARRAY; i++) {
+	var addColor = Math.floor(256 * (i/NARRAY));
+	if (i % 2 == 0)
+	    addColor = Math.floor(256 * 256 * 256 * ((NARRAY-i)/NARRAY));
+	var c = 256 * 125 + addColor;
+	oldMaterials[i] = drawStyleFunctions[drawStyle].makeMaterial(c);
+    }
 
-    {
-      var x = 0;
+    // draw() sets
+    var arrayIdx = 0;
+    var prevVectorArry;
 
-      // var geometry = new THREE.Geometry();
-      
-      // lx closeness check
-      var preLx = -100;
-      var maxLy = 0;
-      var cnt = 0;
-      
-      for(var i = 0; i < maxDrawFreq; i++) {
-        var y = dataArray[i];
+    function draw() {
+	drawVisual = requestAnimationFrame(draw);
 
-	var lx = x;
-	var ly = y;
-
-	// 6.8 =~ Math.log(WIDTH)
-	lx = Math.log(1+x) * WIDTH / 6.8
-        // ly = Math.log(1+barHeight) * 35;
-
-	// skip close log(1+x) positions, pick max y
-	if (lx - preLx >= 1.0) {
-	  vectorArray.push(
-	    new THREE.Vector3(lx, Math.max(maxLy,ly), 0)
-	  );
-	  
-	  preLx = lx;
-	  cnt = 0;
-	  maxLy = 0;
-	} else {
-	  cnt++;
-	  maxLy = Math.max(maxLy, ly);
+	{
+	    // remove old object
+	    var oldObj = objectArray[(arrayIdx + 1)%NARRAY];
+	    if (oldObj) {
+		scene.remove(oldObj);
+		deepDispose(oldObj);
+	    }
+	    // move objects backward
+	    scene.traverse(function(obj) {
+		if(scene.id != obj.id) {
+		    obj.translateZ(ZSTEP);
+		}
+	    });
+	    // change last object material
+	    if (!drawStyleFunctions[drawStyle].skipMaterialChange) {
+      		var prevObj = objectArray[(arrayIdx + NARRAY -1) % NARRAY];
+      		if (prevObj) {
+      		    prevObj.material = oldMaterials[arrayIdx];
+      		}
+	    }
 	}
 	
-        x += unitWidth;
-      }
-      var obj = drawStyleFunctions[drawStyle].makeObject(
-	prevVectorArry,
-	vectorArray,
-	material
-      );
-      if (obj) {
-	objectArray[arrayIdx] = obj;
-	scene.add(obj);
-      }
-    }
-    // console.log('render');
-    renderer.render(scene, camera);
+	analyser.getByteFrequencyData(dataArray);
 
-    prevVectorArry = vectorArray;
-    arrayIdx = (arrayIdx + 1) % NARRAY
-  };
+	var maxDrawFreq = MAX_FREQ / (source.context.sampleRate / analyser.fftSize);
+	maxDrawFreq = Math.min(maxDrawFreq, bufferLength);
+	var unitWidth = (WIDTH / maxDrawFreq);
+	
+	var vectorArray = new Array();
 
-  draw();
+	{
+	    var x = 0;
+
+	    // var geometry = new THREE.Geometry();
+	    
+	    // lx closeness check
+	    var preLx = -100;
+	    var maxLy = 0;
+	    var cnt = 0;
+	    
+	    for(var i = 0; i < maxDrawFreq; i++) {
+		var y = dataArray[i];
+
+		var lx = x;
+		var ly = y;
+
+		// 6.8 =~ Math.log(WIDTH)
+		lx = Math.log(1+x) * WIDTH / 6.8
+		// ly = Math.log(1+barHeight) * 35;
+
+		// skip close log(1+x) positions, pick max y
+		if (lx - preLx >= 1.0) {
+		    vectorArray.push(
+			new THREE.Vector3(lx, Math.max(maxLy,ly), 0)
+		    );
+		    
+		    preLx = lx;
+		    cnt = 0;
+		    maxLy = 0;
+		} else {
+		    cnt++;
+		    maxLy = Math.max(maxLy, ly);
+		}
+		
+		x += unitWidth;
+	    }
+	    var obj = drawStyleFunctions[drawStyle].makeObject(
+		prevVectorArry,
+		vectorArray,
+		material
+	    );
+	    if (obj) {
+		objectArray[arrayIdx] = obj;
+		scene.add(obj);
+	    }
+	}
+	// console.log('render');
+	renderer.render(scene, camera);
+
+	prevVectorArry = vectorArray;
+	arrayIdx = (arrayIdx + 1) % NARRAY
+    };
+
+    draw();
 
 }
 
 // event listeners to change settings
 
 function onchangeFunction() {
-  window.cancelAnimationFrame(drawVisual);
+    window.cancelAnimationFrame(drawVisual);
 
-  if (scene) {
-    var objs = new Array();
-    scene.traverse(function(obj) {
-      if(obj.id != scene.id) objs.push(obj);
-    });
-    var obj;
-    for(obj in objs) {
-      scene.remove(obj);
-      deepDispose(obj);
+    if (scene) {
+	var objs = new Array();
+	scene.traverse(function(obj) {
+	    if(obj.id != scene.id) objs.push(obj);
+	});
+	var obj;
+	for(obj in objs) {
+	    scene.remove(obj);
+	    deepDispose(obj);
+	}
+	scene = undefined;
+	objs = undefined;
     }
-    scene = undefined;
-    objs = undefined;
-  }
-  
-  visualize();
+    
+    visualize();
 }
 
 fftSizeSelect.onchange = onchangeFunction;
-  
+
 nlinesSelect.onchange = onchangeFunction;
 
 styleSelect.onchange = onchangeFunction;
 
 smoothingSelect.onchange = function(e) {
-  var smoothing = Number(smoothingSelect.value);
-  analyser.smoothingTimeConstant = smoothing;
+    var smoothing = Number(smoothingSelect.value);
+    analyser.smoothingTimeConstant = smoothing;
 }
 
 // })();
